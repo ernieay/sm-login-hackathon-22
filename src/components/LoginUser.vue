aa659c3b2e6607b9e804e1be9bfc9b8d7296c8e6
Your name <Your GitHub email>
2022-07-26 15:35:43 +1000

    Added bootstrap and the login form image

---
+++ src/components/LoginUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login-form">
+    <img src="@/assets/LoginForm.png" class="login-form__img" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginUser',
+}
+</script>
+
+<style scoped>
+.login-form__img {
+  width: 440px;
+}
+</style> 
