feb396c8df49a1d8c643076e62119fa56d923f51
Jackono Fabella <jackonofabella@gmail.com>
2022-07-27 13:26:20 +1000

    add easter egg

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,44 @@ export default {
   components: {
     AppHeader,
     LoginUser,
+  },
+  setup: () => {
+    const doABarrelRoll = function(){var a="-webkit-",b='transform:rotate(1turn);',c='transition:4s;';document.head.innerHTML+='<style>body{'+a+b+a+c+b+c}
+
+    const allowedKeys = {
+      68: 'd',
+      69: 'e',
+      73: 'i',
+      77: 'm',
+      78: 'n',
+      82: 'r',
+      83: 's',
+      84: 't',
+    }
+
+    const cheatCode = ['s', 'i', 't', 'e', 'm', 'i', 'n', 'd', 'e', 'r']
+
+    let keysPosition = 0;
+
+    // add keydown event listener
+    window.addEventListener('keydown', function(e) {
+
+      const key = allowedKeys[e.keyCode];
+      const requiredKey = cheatCode[keysPosition];
+
+      // compare the key with the required key
+      if (key == requiredKey) {
+
+        keysPosition++;
+
+        if (keysPosition == cheatCode.length) {
+          doABarrelRoll();
+          keysPosition = 0;
+        }
+      } else {
+        keysPosition = 0;
+      }
+    })
   }
 }
 </script>
